f334ed59fe8a34c4c8c532a1e594f7860d659303
Marta Jaekel <martajaekel@gmail.com>
2024-10-14 16:36:22 +0200

    take out mutable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import { useRef, MutableRefObject } from "react";
+import { useRef} from "react";
 
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
